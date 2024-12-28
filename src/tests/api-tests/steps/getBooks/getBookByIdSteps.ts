890f7aa8c225c3b90cef3f2d719db472d8be9222
Erandhi Madhushika <madhushikabae.20@uom.lk>
2024-12-28 16:54:42 +0530

    Created API test cases for GET book by ID

--- src/tests/api-tests/steps/getBooks/getBookByIdSteps.ts
+++ src/tests/api-tests/steps/getBooks/getBookByIdSteps.ts
@@ -1,16 +1,27 @@
-import { Book } from "../../types/book";
 import { Given, When, Then } from "@cucumber/cucumber";
 import { apiHelper } from "../common/authSteps";
 import { expect } from "playwright/test";
+import { Logger } from "../../../ui-tests/utils/logger";
 
-let bookDetails: Book;
 let response: any;
 
-When("I send a GET request to {string}", async function (endpoint: string) {
-   response = await apiHelper.get(endpoint);
-   console.log(response);
+When('I have the following book ID: {int}', async function (id: number) {
+   response = await apiHelper.get(`/api/books/${id}`);
+   Logger.info("Request Sent");
 });
 
-Then("the response status code should be 200", async function () {
-   expect(response.status).toBe(200);
+Then('the book details should be not be displayed successfully with {int} status code', async function (status: number) {
+   expect(response.status).toBeDefined();
+   expect(response.status).toBe(status);
+   expect(response.data).toBeDefined();
+   Logger.info("Status code: " + response.status);
+   Logger.info("Response data: " + JSON.stringify(response.data));
+});
+
+Then('the book details should not be displayed with {int} status code', async function (status: number) {
+   expect(response.status).toBeDefined();
+   expect(response.status).toBe(status);
+   Logger.info("Status code: " + response.status);
+   expect(response.error).toBeDefined();
+   Logger.info("Response data: " + JSON.stringify(response.error));
 });
--- src/tests/api-tests/utils/apiHelper.ts
+++ src/tests/api-tests/utils/apiHelper.ts
@@ -1,121 +1,119 @@
 import { request } from "playwright";
 
 export class ApiHelper {
-   private baseUrl: string = "http://localhost:7081";
-   private authHeader: string = "";
+  private baseUrl: string = "http://localhost:7081";
+  private authHeader: string = "";
 
-   setBasicAuth(username: string, password: string) {
-      const credentials = Buffer.from(`${username}:${password}`).toString(
-         "base64"
-      );
-      this.authHeader = `Basic ${credentials}`;
-   }
+  setBasicAuth(username: string, password: string) {
+    const credentials = Buffer.from(`${username}:${password}`).toString(
+      "base64"
+    );
+    this.authHeader = `Basic ${credentials}`;
+  }
 
-   async get(endpoint: string) {
-      const context = await request.newContext();
+  async get(endpoint: string) {
+    const context = await request.newContext();
+    let response;
+    try {
+      response = await context.get(`${this.baseUrl}${endpoint}`, {
+        headers: {
+          Authorization: this.authHeader,
+        },
+      });
+      const responseData = {
+        status: response.status(),
+        data: await response.json(),
+      };
+      return responseData;
+    } catch (error: any) {
+      return {
+        status: response?.status(),
+        error: await response?.text(),
+      };
+    } finally {
+      await context.dispose();
+    }
+  }
 
-      try {
-         const response = await context.get(`${this.baseUrl}${endpoint}`, {
-            headers: {
-               Authorization: this.authHeader,
-            },
-         });
-         const responseData = {
-            status: response.status(),
-            data: await response.json(),
-         };
+  async post(endpoint: string, data: any) {
+    const context = await request.newContext();
 
-         return responseData;
-      } catch (error: any) {
-        console.log("API Error:", error);
+    try {
+      const response = await context.post(`${this.baseUrl}${endpoint}`, {
+        headers: {
+          Authorization: this.authHeader,
+          "Content-Type": "application/json",
+        },
+        data: data,
+      });
+
+      const responseData = {
+        status: response.status(),
+        data:
+          response.status() === 201
+            ? await response.json()
+            : await response.text(),
+      };
+
+      return responseData;
+    } catch (error: any) {
+      console.log("API Error:", error);
+      return {
+        status: error.response?.status(),
+        error: (await error.response?.text()) || error.message,
+      };
+    } finally {
+      await context.dispose();
+    }
+  }
+
+  async put(endpoint: string, data: any) {
+    const context = await request.newContext();
+
+    try {
+      const response = await context.put(`${this.baseUrl}${endpoint}`, {
+        headers: {
+          Authorization: this.authHeader,
+          "Content-Type": "application/json",
+        },
+        data: data,
+      });
+
+      const responseData = {
+        status: response.status(),
+        data: await response.json(),
+      };
+
+      return responseData;
+    } catch (error: any) {
+      if (error.response) {
         return {
-           status: error.response?.status(),
-           error: (await error.response?.text()) || error.message,
+          status: error.response.status(),
+          error: await error.response.json(),
         };
-      } finally {
-         await context.dispose();
       }
-   }
+    } finally {
+      await context.dispose();
+    }
+  }
 
-   async post(endpoint: string, data: any) {
-      const context = await request.newContext();
-
-      try {
-         const response = await context.post(`${this.baseUrl}${endpoint}`, {
-            headers: {
-               Authorization: this.authHeader,
-               "Content-Type": "application/json",
-            },
-            data: data,
-         });
-
-         const responseData = {
-            status: response.status(),
-            data:
-               response.status() === 201
-                  ? await response.json()
-                  : await response.text(),
-         };
-
-         return responseData;
-      } catch (error: any) {
-         console.log("API Error:", error);
-         return {
-            status: error.response?.status(),
-            error: (await error.response?.text()) || error.message,
-         };
-      } finally {
-         await context.dispose();
+  async delete(endpoint: string) {
+    const context = await request.newContext();
+    try {
+      const response = await context.delete(`${this.baseUrl}${endpoint}`, {
+        headers: {
+          Authorization: this.authHeader,
+        },
+      });
+    } catch (error: any) {
+      if (error.response) {
+        return {
+          status: error.response.status(),
+          error: await error.response.json(),
+        };
       }
-   }
-
-   async put(endpoint: string, data: any) {
-      const context = await request.newContext();
-
-      try {
-         const response = await context.put(`${this.baseUrl}${endpoint}`, {
-            headers: {
-               Authorization: this.authHeader,
-               "Content-Type": "application/json",
-            },
-            data: data,
-         });
-
-         const responseData = {
-            status: response.status(),
-            data: await response.json(),
-         };
-
-         return responseData;
-      } catch (error: any) {
-         if (error.response) {
-            return {
-               status: error.response.status(),
-               error: await error.response.json(),
-            };
-         }
-      } finally {
-         await context.dispose();
-      }
-   }
-
-   async delete(endpoint: string) {
-      const context = await request.newContext();
-      try {
-         const response = await context.delete(`${this.baseUrl}${endpoint}`, {
-            headers: {
-               Authorization: this.authHeader,
-            },
-         });
-      } catch (error: any) {
-         if (error.response) {
-            return {
-               status: error.response.status(),
-               error: await error.response.json(),
-            };
-         }
-      } finally {
-         await context.dispose();
-      }
-   }
+    } finally {
+      await context.dispose();
+    }
+  }
 }
