bf280657457ac7b230c35fd4bc0129f261c4ad41
Bumuth-Gajakum <87591194+Bumuth-Gajakum@users.noreply.github.com>
2024-12-28 22:45:24 +0530

    formated

--- src/tests/ui-tests/pages/claimModule.ts
+++ src/tests/ui-tests/pages/claimModule.ts
@@ -38,27 +38,18 @@ export class ClaimModule extends BasePage {
  
  async selectCurrencyDropdown() {
     Logger.info("Attempting to select a currency from the dropdown");
- 
-    // Click on the Currency dropdown
     await this.click(this.currencyTypeDropdown, "Currency Type dropdown");
- 
-    // Wait for the dropdown options to become visible
     await this.page.waitForSelector(".oxd-select-dropdown .oxd-select-option", { state: "visible", timeout: 5000 });
     Logger.info("Currency dropdown options are visible");
- 
-    // Add a small wait to ensure dropdown options are fully loaded
     await this.page.waitForTimeout(1000);
- 
-    // Select the first option in the dropdown
     const options = await this.page.locator(".oxd-select-dropdown .oxd-select-option");
-    await options.nth(1).click();  // Select the second option (index 1)
+    await options.nth(1).click(); 
     Logger.info("Currency option selected");
  }
  
   async addRemarks(remarks: string) {
     Logger.info("Waiting for remarks field");
-    await this.page.waitForTimeout(2000); // If needed, use waitForSelector instead
-    Logger.info("Filling in Remarks field");
+    await this.page.waitForTimeout(2000); 
     await this.remarksField.fill(remarks);
     Logger.info("Remarks filled");
  }
