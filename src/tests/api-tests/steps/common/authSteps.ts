b39c8e6a156bb198f0996970e7335473d97fdda1
Navindu <dahanayakenw.20@uom.lk>
2024-12-28 16:20:36 +0530

    updated authStep with config and logger

--- src/tests/api-tests/steps/common/authSteps.ts
+++ src/tests/api-tests/steps/common/authSteps.ts
@@ -1,17 +1,19 @@
 import { Given } from "@cucumber/cucumber";
 import { ApiHelper } from "../../utils/apiHelper";
+import { config } from "../../../../utils/environment";
+import { Logger } from "../../../../utils/logger";
 
 const apiHelper = new ApiHelper();
 
 Given("I am authenticated as {string}", function (userType: string) {
-    const password = "password";
-    if(userType === "admin"){
-        apiHelper.setBasicAuth("admin", password);
-     } else if(userType === "user"){
-         apiHelper.setBasicAuth("user", password);
-     } else {
-         throw new Error("Invalid user type");
-     }
- });
+   if (userType === config.credentials.api.admin.username) {
+      apiHelper.setBasicAuth(config.credentials.api.admin.username);
+   } else if (userType === config.credentials.api.user.username) {
+      apiHelper.setBasicAuth(config.credentials.api.user.username);
+   } else {
+      throw new Error("Invalid user type");
+   }
+   Logger.info(`Authenticated as ${userType}`);
+});
 
- export {apiHelper};
+export { apiHelper };
