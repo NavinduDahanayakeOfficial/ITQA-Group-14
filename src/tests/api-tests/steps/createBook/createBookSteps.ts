778a0ccdaca8c1f25568c75184a1939908a17d47
Navindu <dahanayakenw.20@uom.lk>
2024-12-28 09:24:57 +0530

    added a step for common authStep and updated feature and steps files according to it

--- src/tests/api-tests/steps/createBook/createBookSteps.ts
+++ src/tests/api-tests/steps/createBook/createBookSteps.ts
@@ -1,23 +1,12 @@
 import { Book } from "../../types/book";
 import { Given, When, Then } from "@cucumber/cucumber";
-import { ApiHelper } from "../../utils/apiHelper";
+import { apiHelper } from "../common/authSteps";
 import { expect } from "playwright/test";
 
-const apiHelper = new ApiHelper();
 
 let bookDetails: Book;
 let response: any;
 
-Given("I am authenticated as {string}", function (userType: string) {
-   const password = "password";
-   if(userType === "admin"){
-       apiHelper.setBasicAuth("admin", password);
-    } else if(userType === "user"){
-        apiHelper.setBasicAuth("user", password);
-    } else {
-        throw new Error("Invalid user type");
-    }
-});
 
 Given("I have the following book details:", function (dataTable) {
    bookDetails = dataTable.hashes()[0] as Book;
