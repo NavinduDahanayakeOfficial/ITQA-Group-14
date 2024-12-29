8edae73bc6998f4cd353ce93ba8fc4bcc9809875
TaraJJK <tharakarambukkamage@gmail.com>
2024-12-29 21:11:12 +0530

    Add KPI testcases

--- src/tests/ui-tests/pages/home.page.ts
+++ src/tests/ui-tests/pages/home.page.ts
@@ -4,13 +4,20 @@ import { BasePage } from './base.page';
 export class HomePage extends BasePage {
 
     readonly pimModule: Locator;
+    readonly performanceModule: Locator;
 
     constructor(page: Page) {
         super(page);
         this.pimModule = this.page.locator('//a[normalize-space()="PIM"]');
+        this.performanceModule = this.page.locator('//a[normalize-space()="Performance"]');
+    
     }
 
     async clickPimModule() {
         await this.click(this.pimModule, 'PIM Module');
+    }
+    
+    async clickPerformanceModule() {
+        await this.click(this.performanceModule, 'Performance Module');
     }
 }
