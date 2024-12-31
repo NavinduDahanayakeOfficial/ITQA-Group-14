a55ff44c9d67360a14438df2babe050b335e9eb9
Navindu <dahanayakenw.20@uom.lk>
2025-01-01 01:06:14 +0530

    updated home class

--- src/tests/ui-tests/pages/home.page.ts
+++ src/tests/ui-tests/pages/home.page.ts
@@ -1,24 +1,27 @@
-import { Locator, Page} from 'playwright';
-import { BasePage } from './base.page';
+import { Locator, Page } from "playwright";
+import { BasePage } from "./base.page";
 
 export class HomePage extends BasePage {
+   readonly pimModule: Locator;
+   readonly myInfoModule: Locator;
+   readonly recruitModule: Locator;
 
-    readonly pimModule: Locator;
-    readonly myInfoModule: Locator;
-    readonly recruitModule: Locator;
+   constructor(page: Page) {
+      super(page);
+      this.pimModule = this.page.locator('//a[normalize-space()="PIM"]');
+      this.recruitModule = this.page.locator('//a[normalize-space()="Recruitment"]');
+      this.myInfoModule = this.page.locator('//a[normalize-space()="My Info"]');
+   }
 
-    constructor(page: Page) {
-        super(page);
-        this.pimModule = this.page.locator('//a[normalize-space()="PIM"]');
-        this.myInfoModule = this.page.locator('//a[normalize-space()="My Info"]');
-        this.recruitModule= this.page.locator('//a[normalize-space()="Recruitment"]');
-    }
+   async clickPimModule() {
+      await this.click(this.pimModule, "PIM Module");
+   }
 
-    async clickPimModule() {
-        await this.click(this.pimModule, 'PIM Module');
-    }
+   async clickRecruitmentModule() {
+      await this.click(this.recruitModule, "Recruitment Module");
+   }
 
-    async clickRecruitmentModule() {
-        await this.click(this.recruitModule, 'Recruitment Module');
-    }
+   async clickMyInfoModule() {
+      await this.click(this.myInfoModule, "My Info Module");
+   }
 }
