631dadeec47ed6ecf2652d573f50f0f6f45ab71f
Dasun Piyakantha Geeganage <d.p.geeganage@gmail.com>
2025-01-01 11:03:53 +0530

    get all books when db empty

--- src/tests/api-tests/features/getBooks/getAllBooks.feature
+++ src/tests/api-tests/features/getBooks/getAllBooks.feature
@@ -1,13 +1,18 @@
 @API
 @GetAllBooks
+
 Feature: Get All Books
   As a library user or admin
   I want to fetch the list of books
   So that I can view available books in the system
 
-  Background:
-    Given I am authenticated as "user"
-
-  Scenario: Successfully fetching the list of books
+  Scenario: Successfully fetching the list of books as Admin
+    Given I am authenticated as "admin"
     When I fetch the list of books
     Then the API should return a list of books
+
+  Scenario: Successfully fetching the list of books as User
+    Given I am authenticated as "user"
+    When I fetch the list of books
+    Then the API should return a list of books
+
--- src/tests/api-tests/steps/getBooks/getAllBooksSteps.ts
+++ src/tests/api-tests/steps/getBooks/getAllBooksSteps.ts
@@ -6,24 +6,23 @@ import { Logger } from "../../../../utils/logger";
 let response: any;
 
 When("I fetch the list of books", async function () {
-    response = await apiHelper.get("/api/books");
-    Logger.info("Send GET request to fetch the list of books");
- });
- 
- Then("the API should return a list of books", function () {
-    Logger.info("If successful, the response status should be 200");
-    Logger.info("Response status: " + response.status);
-    expect(response).toBeDefined();
-    expect(response.status).toBe(200);
- 
-    Logger.info("Response data should be an array of books");
-    expect(response.data).toBeInstanceOf(Array);
-    if (response.data.length > 0) {
-       response.data.forEach((book: any) => {
-          expect(book).toHaveProperty("id");
-          expect(book).toHaveProperty("title");
-          expect(book).toHaveProperty("author");
-       });
-    }
- });
- 
+   response = await apiHelper.get("/api/books");
+   Logger.info("Send GET request to fetch the list of books");
+});
+
+Then("the API should return a list of books", function () {
+   Logger.info("If successful, the response status should be 200");
+   Logger.info("Response status: " + response.status);
+   expect(response).toBeDefined();
+   expect(response.status).toBe(200);
+
+   Logger.info("Response data should be an array of books");
+   expect(response.data).toBeInstanceOf(Array);
+   if (response.data.length > 0) {
+      response.data.forEach((book: any) => {
+         expect(book).toHaveProperty("id");
+         expect(book).toHaveProperty("title");
+         expect(book).toHaveProperty("author");
+      });
+   }
+});
