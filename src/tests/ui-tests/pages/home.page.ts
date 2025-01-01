5c13c23484675074ce8fdd94f248ab2b14e1c2d6
Rushen Samodya <rushenssamodya@gmail.com>
2025-01-01 21:40:25 +0530

    added the buzz feed based UI test cases

--- src/tests/ui-tests/pages/home.page.ts
+++ src/tests/ui-tests/pages/home.page.ts
@@ -6,13 +6,14 @@ export class HomePage extends BasePage {
     readonly pimModule: Locator;
     readonly recruitModule: Locator;
     readonly performanceModule: Locator;
+    readonly buzzModule: Locator;
 
     constructor(page: Page) {
         super(page);
         this.pimModule = this.page.locator('//a[normalize-space()="PIM"]');
         this.recruitModule= this.page.locator('//a[normalize-space()="Recruitment"]');
         this.performanceModule = this.page.locator('//a[normalize-space()="Performance"]');
-    
+        this.buzzModule = this.page.locator('//a[normalize-space()="Buzz"]');
     }
 
     async clickPimModule() {
@@ -25,5 +26,10 @@ export class HomePage extends BasePage {
     
     async clickPerformanceModule() {
         await this.click(this.performanceModule, 'Performance Module');
+    }
+
+    async clickBuzzModule() {
+        await this.click(this.buzzModule, 'Buzz Module');
+        await this.page.waitForTimeout(4000);
     }
 }
---
+++ src/tests/ui-tests/steps/deletePost.ts
@@ -0,0 +1,28 @@
+import { Given, When, Then } from "@cucumber/cucumber";
+import { page } from "../pages/hooks";
+import { expect } from "@playwright/test";
+import { Logger } from "../../../utils/logger";
+import { BuzzModule } from "../pages/buzzModule";
+
+let buzzModule: BuzzModule;
+
+When("User clicks the select options button", async function () {
+    buzzModule = new BuzzModule(page);
+    await buzzModule.clickSelectOptionsButton();
+    Logger.info('Clicked the select options button');
+});
+
+When('User clicks the delete button', async function () {
+    await buzzModule.clickDeleteButton();
+    Logger.info('Clicked the delete button');
+});
+
+When('User clicks the confirm button', async function () {
+    await buzzModule.clickConfirmButton();
+    Logger.info('Clicked the confirm button');
+});
+
+Then('User should not see the post displayed in the Buzz Newsfeed', async function (text) {
+    await buzzModule.verifyPostDeleted();
+    Logger.info('Post is deleted from the Buzz Newsfeed');
+});
