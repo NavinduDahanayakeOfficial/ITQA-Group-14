5d04bc9f377b2c0afabdc46534d753c29741ca90
Venushki Thilakawardana <thilakawardanavn.20@uom.lk>
2025-01-01 22:55:36 +0530

    Apply leave and Assign Leave

--- src/tests/ui-tests/pages/home.page.ts
+++ src/tests/ui-tests/pages/home.page.ts
@@ -1,29 +1,32 @@
-import { Locator, Page} from 'playwright';
-import { BasePage } from './base.page';
+import { Locator, Page } from "playwright";
+import { BasePage } from "./base.page";
 
 export class HomePage extends BasePage {
+  readonly pimModule: Locator;
+  readonly recruitModule: Locator;
+  readonly performanceModule: Locator;
+  readonly leaveModule: Locator;
 
-    readonly pimModule: Locator;
-    readonly recruitModule: Locator;
-    readonly performanceModule: Locator;
+  constructor(page: Page) {
+    super(page);
+    this.pimModule = this.page.locator('//a[normalize-space()="PIM"]');
+    this.recruitModule = this.page.locator('//a[normalize-space()="Recruitment"]');
+    this.performanceModule = this.page.locator('//a[normalize-space()="Performance"]');
+    this.leaveModule = this.page.locator('//a[normalize-space()="Leave"]');
+  }
 
-    constructor(page: Page) {
-        super(page);
-        this.pimModule = this.page.locator('//a[normalize-space()="PIM"]');
-        this.recruitModule= this.page.locator('//a[normalize-space()="Recruitment"]');
-        this.performanceModule = this.page.locator('//a[normalize-space()="Performance"]');
-    
-    }
+  async clickPimModule() {
+    await this.click(this.pimModule, "PIM Module");
+  }
 
-    async clickPimModule() {
-        await this.click(this.pimModule, 'PIM Module');
-    }
+  async clickRecruitmentModule() {
+    await this.click(this.recruitModule, "Recruitment Module");
+  }
 
-    async clickRecruitmentModule() {
-        await this.click(this.recruitModule, 'Recruitment Module');
-    }
-    
-    async clickPerformanceModule() {
-        await this.click(this.performanceModule, 'Performance Module');
-    }
+  async clickPerformanceModule() {
+    await this.click(this.performanceModule, "Performance Module");
+  }
+  async clickLeaveModule() {
+    await this.click(this.leaveModule, "Leave Module");
+  }
 }
---
+++ src/tests/ui-tests/pages/leaveModule.ts
@@ -0,0 +1,87 @@
+import { Locator, Page } from "playwright";
+import { BasePage } from "./base.page";
+
+import { takeCoverage } from "v8";
+import { Logger } from "../../../utils/logger";
+
+export class LeaveModule extends BasePage {
+  readonly applyLeaveButton: Locator;
+  readonly leaveTypeDropdown: Locator;
+  readonly fromDateField: Locator;
+  readonly toDateField: Locator;
+  readonly commentsField: Locator;
+  readonly applyButton: Locator;
+  readonly employeeNameField: Locator;
+  readonly assignLeaveButton: Locator;
+  readonly assignButton: Locator;
+
+  constructor(page: Page) {
+    super(page);
+
+    // Locators for the leave module
+    this.applyLeaveButton = this.page.locator('//a[normalize-space()="Apply"]');
+    this.leaveTypeDropdown = this.page.locator('//div[contains(@class, "oxd-select-wrapper")]');
+    this.fromDateField = this.page.locator('(//div[contains(@class, "oxd-date-input")]//input)[1]');
+    this.toDateField = this.page.locator('(//div[contains(@class, "oxd-date-input")]//input)[2]');
+    this.commentsField = this.page.locator('//textarea[contains(@class, "oxd-textarea")]');
+    this.applyButton = this.page.locator('//button[normalize-space()="Apply"]');
+    this.employeeNameField = page.locator("//input[@placeholder='Type for hints...']");
+    this.assignLeaveButton = page.locator('//a[normalize-space()="Assign Leave"]');
+    this.assignButton = page.locator("//button[@type='submit']");
+  }
+
+  // Navigate to the Leave module
+  async navigateToLeaveModule() {
+    Logger.info("Navigating to the Leave module");
+    await this.applyLeaveButton.click();
+  }
+
+  // Click the 'Apply Leave' button
+  async clickApplyLeave() {
+    await this.click(this.applyLeaveButton, "Apply Leave button");
+  }
+
+  // Select an option from a dropdown
+  async selectDropdown() {
+    await this.click(this.leaveTypeDropdown, "Leave Type dropdown");
+    await this.page.waitForSelector(".oxd-select-dropdown .oxd-select-option", { state: "visible" });
+    await this.page.waitForTimeout(1000);
+    await this.page.locator(".oxd-select-dropdown .oxd-select-option").nth(1).click();
+  }
+
+  async selectFromDate(date: string) {
+    await this.page.waitForTimeout(2000);
+    await this.fromDateField.clear();
+    await this.fromDateField.fill(date);
+  }
+
+  async selectToDate(date: string) {
+    await this.page.waitForTimeout(2000);
+    await this.toDateField.clear();
+    await this.toDateField.fill(date);
+  }
+
+  async addComments(comments: string) {
+    await this.page.waitForTimeout(2000);
+    await this.commentsField.fill(comments);
+  }
+
+  // Click the 'Apply' button
+  async clickApplyButton() {
+    await this.click(this.applyButton, "Apply button");
+  }
+  async enterEmployeeName(name: string) {
+    await this.page.waitForTimeout(2000);
+    await this.employeeNameField.fill(name);
+  }
+
+  async clickAssignLeave() {
+    await this.page.waitForTimeout(2000);
+    await this.assignLeaveButton.click();
+  }
+
+  async clickAssignButton() {
+    await this.page.waitForTimeout(2000);
+    await this.assignButton.click();
+  }
+}
