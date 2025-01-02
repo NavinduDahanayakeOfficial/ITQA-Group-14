efbd240ab41473324abe08bb7d30a0a4cb75c793
Navindu <dahanayakenw.20@uom.lk>
2025-01-02 22:07:36 +0530

    updated homePage

--- src/tests/ui-tests/pages/home.page.ts
+++ src/tests/ui-tests/pages/home.page.ts
@@ -2,38 +2,47 @@ import { Locator, Page } from "playwright";
 import { BasePage } from "./base.page";
 
 export class HomePage extends BasePage {
-  readonly pimModule: Locator;
-  readonly claimModule: Locator;
-  readonly myInfoModule: Locator;
-  readonly recruitModule: Locator;
-  readonly performanceModule: Locator;
-  readonly leaveModule: Locator;
+   readonly pimModule: Locator;
+   readonly claimModule: Locator;
+   readonly myInfoModule: Locator;
+   readonly recruitModule: Locator;
+   readonly performanceModule: Locator;
+   readonly leaveModule: Locator;
 
-  constructor(page: Page) {
-    super(page);
-    this.pimModule = this.page.locator('//a[normalize-space()="PIM"]');
-    this.claimModule = this.page.locator('//a[normalize-space()="Claim"]');
-    this.myInfoModule = this.page.locator('//a[normalize-space()="My Info"]');
-    this.recruitModule = this.page.locator('//a[normalize-space()="Recruitment"]');
-    this.performanceModule = this.page.locator('//a[normalize-space()="Performance"]');
-    this.leaveModule = this.page.locator('//a[normalize-space()="Leave"]');
-  }
+   constructor(page: Page) {
+      super(page);
+      this.pimModule = this.page.locator('//a[normalize-space()="PIM"]');
+      this.claimModule = this.page.locator('//a[normalize-space()="Claim"]');
+      this.myInfoModule = this.page.locator('//a[normalize-space()="My Info"]');
+      this.recruitModule = this.page.locator(
+         '//a[normalize-space()="Recruitment"]'
+      );
+      this.performanceModule = this.page.locator(
+         '//a[normalize-space()="Performance"]'
+      );
+      this.leaveModule = this.page.locator('//a[normalize-space()="Leave"]');
+   }
 
-  async clickPimModule() {
-    await this.click(this.pimModule, "PIM Module");
-  }
-  async clickClaimModule() {
-    await this.click(this.claimModule, "Claim Module");
-  }
+   async clickPimModule() {
+      await this.click(this.pimModule, "PIM Module");
+   }
 
-  async clickRecruitmentModule() {
-    await this.click(this.recruitModule, "Recruitment Module");
-  }
+   async clickClaimModule() {
+      await this.click(this.claimModule, "Claim Module");
+   }
 
-  async clickPerformanceModule() {
-    await this.click(this.performanceModule, "Performance Module");
-  }
-  async clickLeaveModule() {
-    await this.click(this.leaveModule, "Leave Module");
-  }
+   async clickMyInfoModule() {
+      await this.click(this.myInfoModule, "My Info Module");
+   }
+
+   async clickRecruitmentModule() {
+      await this.click(this.recruitModule, "Recruitment Module");
+   }
+
+   async clickPerformanceModule() {
+      await this.click(this.performanceModule, "Performance Module");
+   }
+   async clickLeaveModule() {
+      await this.click(this.leaveModule, "Leave Module");
+   }
 }
