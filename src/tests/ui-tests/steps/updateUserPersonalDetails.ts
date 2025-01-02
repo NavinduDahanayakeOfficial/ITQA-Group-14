0060f4499721e99a8eedbdcbe85ad6ed835b325a
Navindu <dahanayakenw.20@uom.lk>
2025-01-02 22:08:48 +0530

    implemented updateUserPersonalInfo step file

---
+++ src/tests/ui-tests/steps/updateUserPersonalDetails.ts
@@ -0,0 +1,54 @@
+import { Given, When, Then } from "@cucumber/cucumber";
+import { page } from "../pages/hooks";
+import { expect } from "@playwright/test";
+import { MyInfoModule } from "../pages/myInfoModule";
+import { PersonalInfoSection } from "../pages/myInfoModule";
+import { Logger } from "../../../utils/logger";
+
+let myInfoModule: MyInfoModule;
+let personalInfoSection: PersonalInfoSection;
+
+let data: any;
+
+When(
+   "User enter the new personal details",
+   async function (dataTable: { hashes: () => any }) {
+      personalInfoSection = new PersonalInfoSection(page);
+      data = dataTable.hashes()[0];
+      Logger.info("Filling in the personal details");
+      Logger.info(JSON.stringify(data));
+      await personalInfoSection.fillPersonalInfo(data);
+   }
+);
+
+When("User clicks on the Save button", async function () {
+   await personalInfoSection.savePersonalInfo();
+});
+
+Then(
+   "User should see the success message {string}",
+   async function (expectedMessage: string) {
+      const message = await personalInfoSection.getSuccessMessage();
+      expect(message).toBe(expectedMessage);
+   }
+);
+
+Then("User should see the updated personal details", async function () {
+   await page.reload();
+   await page.waitForTimeout(2000);
+
+   const updatedDetails = await personalInfoSection.getPersonalInfo();
+   console.log(updatedDetails);
+   console.log(data);
+
+   expect(updatedDetails.firstName).toBe(data.firstName);
+   expect(updatedDetails.lastName).toBe(data.lastName);
+   expect(updatedDetails.middleName).toBe(data.middleName);
+   expect(updatedDetails.otherId).toBe(data.otherId);
+   expect(updatedDetails.licenseNumber).toBe(data.licenseNumber);
+   expect(updatedDetails.licenseExpiry).toBe(data.licenseExpiry);
+   expect(updatedDetails.maritalStatus).toBe(data.maritalStatus);
+   expect(updatedDetails.dateOfBirth).toBe(data.dateOfBirth);
+   expect(updatedDetails.gender).toBe(data.gender);
+});
+
